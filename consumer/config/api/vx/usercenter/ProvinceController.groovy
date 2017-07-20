6bea1d9262543c9cf228dc31f9dcc06af8c013a3
addday <addday@gmail.com>
2017-07-20 09:51:40 +0800

    优化性能

---
+++ consumer/config/api/vx/usercenter/ProvinceController.groovy
@@ -0,0 +1,27 @@
+package top.cardone.api.vx.usercenter
+
+import top.cardone.usercenter.service.ProvinceService
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import top.cardone.context.ApplicationContextHolder
+import top.cardone.web.support.WebSupport
+
+import javax.servlet.http.HttpServletRequest
+
+/**
+ * Created by yht
+ */
+@Controller
+@RequestMapping("/vx/usercenter/province")
+class ProvinceController {
+    /**
+     * /c0001.json begin
+     **xx/
+    @RequestMapping("/c0001.json")
+    @ResponseBody
+    Object c0001Json(HttpServletRequest request) {
+        ApplicationContextHolder.getBean(WebSupport.class).func(request, { input -> ApplicationContextHolder.getBean(ProvinceService.class).insertByNotExistsCache(input) })
+    }
+    /** /c0001.json end **/
+}
