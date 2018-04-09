b7ce659fafb36a92f987f93a05db831644081d3e
addday <addday@gmail.com>
2018-04-09 11:41:29 +0800

    配置优化：减少 ${app.root}变量的使用，改进为使用file:

--- api/src/main/java/top/cardone/usercenter/service/AreaService.java
+++ api/src/main/java/top/cardone/usercenter/service/AreaService.java
@@ -1,9 +1,7 @@
 package top.cardone.usercenter.service;
 
 import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
 import org.springframework.transaction.annotation.Transactional;
 import top.cardone.cache.Caches;
 import top.cardone.context.annotation.Event;
@@ -12,7 +10,6 @@ import top.cardone.context.event.SimpleErrorEvent;
 import top.cardone.context.event.SimpleEvent;
 import top.cardone.data.service.PageService;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,194 +22,6 @@ import java.util.Map;
 @Events({@Event(applicationEvent = SimpleErrorEvent.class),
         @Event(applicationEvent = SimpleEvent.class)})
 public interface AreaService extends PageService {
-	/**
-     * @see AreaService#page
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default Page<Map<String, Object>> pageCache(Object page) {
-        return this.page(page);
-    }
-
-	/**
-     * @see AreaService#page
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <P> Page<P> pageCache(Class<P> mappedClass, Object page) {
-        return this.page(mappedClass, page);
-    }
-
-	/**
-     * @see AreaService#findList
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <P> List<P> findListCache(Class<P> mappedClass, Object findList) {
-        return this.findList(mappedClass, findList);
-    }
-
-	/**
-     * @see AreaService#findOne
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <P> P findOneCache(Class<P> mappedClass, Object findOne) {
-        return this.findOne(mappedClass, findOne);
-    }
-
-	/**
-     * @see AreaService#readList
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <R> List<R> readListCache(Class<R> requiredType, Object readList) {
-        return this.readList(requiredType, readList);
-    }
-
-	/**
-     * @see AreaService#readOne
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <R> R readOneCache(Class<R> requiredType, Object readOne) {
-        return this.readOne(requiredType, readOne);
-    }
-
-	/**
-     * @see AreaService#delete
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int deleteCache(Object delete) {
-        return this.delete(delete);
-    }
-
-	/**
-     * @see AreaService#deleteAll
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int deleteAllCache() {
-        return this.deleteAll();
-    }
-
-	/**
-     * @see AreaService#deleteByIds
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int deleteByIdsCache(Object ids) {
-        return this.deleteByIds(ids);
-    }
-
-	/**
-     * @see AreaService#deleteList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] deleteListCache(List<Object> deleteList) {
-        return this.deleteList(deleteList);
-    }
-
-	/**
-     * @see AreaService#findList
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default List<Map<String, Object>> findListCache(Object findList) {
-        return this.findList(findList);
-    }
-
-	/**
-     * @see AreaService#findOne
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default Map<String, Object> findOneCache(Object findOne) {
-        return this.findOne(findOne);
-    }
-
-	/**
-     * @see AreaService#insert
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int insertCache(Object insert) {
-        return this.insert(insert);
-    }
-
-	/**
-     * @see AreaService#insertByNotExists
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int insertByNotExistsCache(Object insert) {
-        return this.insertByNotExists(insert);
-    }
-
-	/**
-     * @see AreaService#insertList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] insertListCache(List<Object> insertList) {
-        return this.insertList(insertList);
-    }
-
-	/**
-     * @see AreaService#insertListByNotExists
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] insertListByNotExistsCache(List<Object> insertList) {
-        return this.insertListByNotExists(insertList);
-    }
-
-	/**
-     * @see AreaService#readList
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default List<Object> readListCache(Object readList) {
-        return this.readList(readList);
-    }
-
-	/**
-     * @see AreaService#readOne
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default Object readOneCache(Object readOne) {
-        return this.readOne(readOne);
-    }
-
-	/**
-     * @see AreaService#save
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int saveCache(Object save) {
-        return this.save(save);
-    }
-
-	/**
-     * @see AreaService#update
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int updateCache(Object update) {
-        return this.update(update);
-    }
-
-	/**
-     * @see AreaService#updateList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] updateListCache(List<Object> updateList) {
-        return this.updateList(updateList);
-    }
-
-	/**
-     * @see AreaService#saveList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] saveListCache(List<Object> saveList) {
-        return this.saveList(saveList);
-    }
-	
     /**
      * 查询地区对象
      *
@@ -220,25 +29,15 @@ public interface AreaService extends PageService {
      * @return 地区对象
      */
     Map<String, Object> findOneByAreaId(Map<String, Object> findOne);
-	
+
     /**
      * 查询地区对象
      *
      * @param findOne 地区标识
      * @return 地区对象
      */
+    @Cacheable(key = Caches.KEY_1)
     default Map<String, Object> findOneByAreaIdCache(Map<String, Object> findOne) {
         return this.findOneByAreaId(findOne);
-    }
-	
-    /**
-     * 查询地区下拉列表
-     *
-     * @param findList 关键字
-     * @return 地区下拉列表
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default List<Map<String, Object>> findListByKeywordCache(Map<String, Object> findList) {
-        return this.findListByKeyword(findList);
     }
 }
--- api/src/main/java/top/cardone/usercenter/service/CityService.java
+++ api/src/main/java/top/cardone/usercenter/service/CityService.java
@@ -1,9 +1,7 @@
 package top.cardone.usercenter.service;
 
 import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
 import org.springframework.transaction.annotation.Transactional;
 import top.cardone.cache.Caches;
 import top.cardone.context.annotation.Event;
@@ -12,7 +10,6 @@ import top.cardone.context.event.SimpleErrorEvent;
 import top.cardone.context.event.SimpleEvent;
 import top.cardone.data.service.PageService;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,194 +22,6 @@ import java.util.Map;
 @Events({@Event(applicationEvent = SimpleErrorEvent.class),
         @Event(applicationEvent = SimpleEvent.class)})
 public interface CityService extends PageService {
-	/**
-     * @see CityService#page
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default Page<Map<String, Object>> pageCache(Object page) {
-        return this.page(page);
-    }
-
-	/**
-     * @see CityService#page
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <P> Page<P> pageCache(Class<P> mappedClass, Object page) {
-        return this.page(mappedClass, page);
-    }
-
-	/**
-     * @see CityService#findList
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <P> List<P> findListCache(Class<P> mappedClass, Object findList) {
-        return this.findList(mappedClass, findList);
-    }
-
-	/**
-     * @see CityService#findOne
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <P> P findOneCache(Class<P> mappedClass, Object findOne) {
-        return this.findOne(mappedClass, findOne);
-    }
-
-	/**
-     * @see CityService#readList
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <R> List<R> readListCache(Class<R> requiredType, Object readList) {
-        return this.readList(requiredType, readList);
-    }
-
-	/**
-     * @see CityService#readOne
-     */
-    @Cacheable(key = Caches.KEY_2)
-    default <R> R readOneCache(Class<R> requiredType, Object readOne) {
-        return this.readOne(requiredType, readOne);
-    }
-
-	/**
-     * @see CityService#delete
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int deleteCache(Object delete) {
-        return this.delete(delete);
-    }
-
-	/**
-     * @see CityService#deleteAll
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int deleteAllCache() {
-        return this.deleteAll();
-    }
-
-	/**
-     * @see CityService#deleteByIds
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int deleteByIdsCache(Object ids) {
-        return this.deleteByIds(ids);
-    }
-
-	/**
-     * @see CityService#deleteList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] deleteListCache(List<Object> deleteList) {
-        return this.deleteList(deleteList);
-    }
-
-	/**
-     * @see CityService#findList
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default List<Map<String, Object>> findListCache(Object findList) {
-        return this.findList(findList);
-    }
-
-	/**
-     * @see CityService#findOne
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default Map<String, Object> findOneCache(Object findOne) {
-        return this.findOne(findOne);
-    }
-
-	/**
-     * @see CityService#insert
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int insertCache(Object insert) {
-        return this.insert(insert);
-    }
-
-	/**
-     * @see CityService#insertByNotExists
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int insertByNotExistsCache(Object insert) {
-        return this.insertByNotExists(insert);
-    }
-
-	/**
-     * @see CityService#insertList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] insertListCache(List<Object> insertList) {
-        return this.insertList(insertList);
-    }
-
-	/**
-     * @see CityService#insertListByNotExists
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] insertListByNotExistsCache(List<Object> insertList) {
-        return this.insertListByNotExists(insertList);
-    }
-
-	/**
-     * @see CityService#readList
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default List<Object> readListCache(Object readList) {
-        return this.readList(readList);
-    }
-
-	/**
-     * @see CityService#readOne
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default Object readOneCache(Object readOne) {
-        return this.readOne(readOne);
-    }
-
-	/**
-     * @see CityService#save
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int saveCache(Object save) {
-        return this.save(save);
-    }
-
-	/**
-     * @see CityService#update
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int updateCache(Object update) {
-        return this.update(update);
-    }
-
-	/**
-     * @see CityService#updateList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] updateListCache(List<Object> updateList) {
-        return this.updateList(updateList);
-    }
-
-	/**
-     * @see CityService#saveList
-     */
-    @CacheEvict(allEntries = true)
-    @Transactional
-    default int[] saveListCache(List<Object> saveList) {
-        return this.saveList(saveList);
-    }
-	
     /**
      * 查询城市对象
      *
@@ -220,25 +29,15 @@ public interface CityService extends PageService {
      * @return 城市对象
      */
     Map<String, Object> findOneByCityId(Map<String, Object> findOne);
-	
+
     /**
      * 查询城市对象
      *
      * @param findOne 城市标识
      * @return 城市对象
      */
+    @Cacheable(key = Caches.KEY_1)
     default Map<String, Object> findOneByCityIdCache(Map<String, Object> findOne) {
         return this.findOneByCityId(findOne);
-    }
-	
-    /**
-     * 查询城市下拉列表
-     *
-     * @param findList 关键字
-     * @return 城市下拉列表
-     */
-    @Cacheable(key = Caches.KEY_1)
-    default List<Map<String, Object>> findListByKeywordCache(Map<String, Object> findList) {
-        return this.findListByKeyword(findList);
     }
 }
