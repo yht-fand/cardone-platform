c89b77d58b665e76cdb4f6cbc10091fa22f34a8d
addday@gmail.com <addday@gmail.com>
2018-09-17 10:42:23 +0800

    改进：某个用户权限清理问题

--- consumer/config/cardone/groovy/top/cardone/usercenter/action/EvictUserCacheAction.groovy
+++ consumer/config/cardone/groovy/top/cardone/usercenter/action/EvictUserCacheAction.groovy
@@ -1,5 +1,7 @@
 package top.cardone.usercenter.action
 
+import org.apache.commons.lang.StringUtils
+import top.cardone.authority.service.UserPermissionService
 import top.cardone.cache.Cache
 import top.cardone.context.ApplicationContextHolder
 import top.cardone.core.util.action.Action1
@@ -7,6 +9,10 @@ import top.cardone.core.util.action.Action1
 class EvictUserCacheAction implements Action1<String> {
     @Override
     void action(String userCode) {
+        if (StringUtils.isBlank(userCode)) {
+            return
+        }
+
         ApplicationContextHolder.getBean(Cache.class).evict("top.cardone.authority.service.UserPermissionService",
                 "readListPermissionCodeByUserCodeCache(" + userCode + ")")
 
@@ -16,10 +22,22 @@ class EvictUserCacheAction implements Action1<String> {
         ApplicationContextHolder.getBean(Cache.class).evict("com.xa.rdmp.authority.func.ReadListPermissionCodeForMainSiteFunc",
                 userCode)
 
-        ApplicationContextHolder.getBean(Cache.class).evict("top.cardone.authority.service.ReadListRoleCodeForMainSiteFunc",
+        ApplicationContextHolder.getBean(Cache.class).evict("com.xa.rdmp.authority.func.ReadListRoleCodeForMainSiteFunc",
                 userCode)
 
         ApplicationContextHolder.getBean(Cache.class).evict("top.cardone.configuration.service.NavigationService",
-                "findListForTreeCache({userCode=" + userCode + ", notId=notId, appendConfigValueKey=select})")
+                "findListForTreeCache(" +
+                        org.springframework.util.StringUtils.arrayToCommaDelimitedString(["userCode": userCode, "notId": "notId", "appendConfigValueKey": "select"]) +
+                        ")")
+
+//        ApplicationContextHolder.getBean(Cache.class).clear("top.cardone.configuration.service.NavigationService")
+
+        ApplicationContextHolder.getBean(Cache.class).evict(UserPermissionService.class.getName(), "navigation:view:," + userCode)
+
+        ApplicationContextHolder.getBean(Cache.class).evict(UserPermissionService.class.getName(), "site:view:," + userCode)
+
+        ApplicationContextHolder.getBean(Cache.class).evict(UserPermissionService.class.getName(), "department:view:," + userCode)
+
+        ApplicationContextHolder.getBean(Cache.class).evict(UserPermissionService.class.getName(), "user:view:," + userCode)
     }
 }
